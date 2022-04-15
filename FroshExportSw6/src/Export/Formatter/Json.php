80dbe3378e39bd66e18ac381c473f8b69e4878d8
En0Ma1259 <fboensch0@gmail.com>
2022-04-15 11:00:04 +0200

    Add files via upload

---
+++ FroshExportSw6/src/Export/Formatter/Json.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Frosh\Exporter\Export\Formatter;
+
+use Frosh\Exporter\Entity\FroshExportEntity;
+
+class Json extends AbstractFormatter
+{
+    public static function fileExtension(): string
+    {
+        return 'json';
+    }
+
+    public function startFile(FroshExportEntity $exportEntity): void
+    {
+        parent::startFile($exportEntity);
+
+        $this->filesystem->put($this->getFilename(), '[');
+    }
+
+    public function endFile(FroshExportEntity $exportEntity): void
+    {
+        // TODO: Remove last ',' and empty element '{}'
+        $this->filesystem->put($this->getFilename(), '{}]');
+
+        parent::endFile($exportEntity);
+    }
+
+    public function writeItem($item): void
+    {
+        $this->filesystem->put($this->getFilename(), json_encode($item, JSON_THROW_ON_ERROR) . ',');
+    }
+}
